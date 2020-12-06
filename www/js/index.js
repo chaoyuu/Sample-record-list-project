e7f2a468b8187cc20f5245db00d6865e85837b6f
unknown <6112@ait.nsw.edu.au>
2020-12-07 02:50:13 +1100

    update

---
+++ www/js/index.js
@@ -0,0 +1,57 @@
+const Position = require("../../plugins/cordova-plugin-geolocation/www/Position");
+
+document.addEventListener('deviceready', yourOwnFunction, false);
+
+window.addEventListener("batterystatus", onBatteryStatus, false);
+
+navigator.geolocation.getCurrentPosition(whenLocationObtained, whenLocationError);
+
+
+
+function whenLocationObtained() {
+  alert("latitude : " + position.coords.latitude + '\n' +
+    'Longitude: ' + position.coords.longitude)
+}
+
+function whenLocationError(error) {
+  alert('code: ' + error.code + '\n' +
+    'message: ' + error.message + '\n');
+}
+function yourOwnFunction() {
+
+  console.log("Cordova Device is ready!!")
+
+  // login_btn listenr
+  $('#login_btn').click(login)
+
+  $('#save_btn').click(registerNewUser)
+
+}
+
+function registerNewUser() {
+  let userName = $('#newUserName').val()
+  let password = $('#newPassword').val()
+
+  localStorage.setItem("userName", userName)
+  localStorage.setItem("password", password)
+
+  window.location.href = "#login"
+}
+function onBatteryStatus(status) {
+  console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
+}
+
+function login() {
+
+
+  let userName = $('#userName').val()
+  let password = $('#password').val()
+
+  if (userName == localStorage.getItem('userName') && password == localStorage.getItem('password')) {
+    // home
+    window.location.href = "#home"
+    $('#welcome_msg').text('Welcom ' + userName)
+  } else {
+    alert("Invalid username or password, please try again.")
+  }
+}
